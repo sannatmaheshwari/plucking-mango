1ba4418e8b7fd4705c9bfc6d024acc4377293f24
sannatmaheshwari <79201938+sannatmaheshwari@users.noreply.github.com>
2021-05-06 17:38:59 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,27 @@
+class launcher{
+constructor(bodyA,pointB){
+var options = {
+bodyA: bodyA,
+pointB: pointB,
+stiffness: 0.04,
+length: 10
+
+}
+this.pointB = pointB  
+this.launcher = Constraint.create(options);
+World.add(world, this.launcher);
+
+}
+attach(body){
+    this.launcher.bodyA = body;
+}
+
+fly(){
+    this.launcher.bodyA = null;
+}
+display(){
+    strokeWeight(3)
+}
+
+
+}
